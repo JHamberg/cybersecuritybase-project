d4df12f243e99d09f5c2bca46468ad50ec6d7480
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2017-01-24 23:34:42 +0200

    Add sql schema and import queries

---
+++ sql/database-import.sql
@@ -0,0 +1,2 @@
+INSERT INTO Users (id, name, reason) VALUES (1, 'Matt Mattson', 'Bad cough');
+INSERT INTO Users (id, name, reason) VALUES (2, 'Cacey Macey', 'Stress');
---
+++ sql/database-schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Users (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(200),
+    reason varchar(200),
+    PRIMARY KEY (id)
+);
